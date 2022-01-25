bf2d7ce231f548cfc5bce8c7c10ba0b1339a9c43
jefc21 <jefc21@gmail.com>
2022-01-24 22:55:11 -0300

    routes e

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,11 @@ class StaticPagesController < ApplicationController
     def index
         @nome="eric_costa"
     end
+
+    def sobre     
+    end
+
+    def contato
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "static_pages#index"
 
+  get 'sobre', to: 'static_pages#sobre'
+
+  get 'contato', to: 'static_pages#contato'
+
 end
